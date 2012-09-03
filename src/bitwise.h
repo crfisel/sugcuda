cbfa1533f684177769d4360f9ad28b8d392dd341
C. Richard Fisel <crfpub@gmail.com>
2012-09-03 10:31:41 -0500

    fix the unpacking of cudpp random bits 12-20-11

--- src/bitwise.h
+++ src/bitwise.h
@@ -41,4 +41,44 @@ union GridBitWise
     	int asInt;
 };
 
+union BitUnpacker
+{
+	struct BitMask
+	{
+		unsigned short b1 : 1;
+		unsigned short b2 : 1;
+		unsigned short b3 : 1;
+		unsigned short b4 : 1;
+		unsigned short b5 : 1;
+		unsigned short b6 : 1;
+		unsigned short b7 : 1;
+		unsigned short b8 : 1;
+		unsigned short b9 : 1;
+		unsigned short b10 : 1;
+		unsigned short b11 : 1;
+		unsigned short b12 : 1;
+		unsigned short b13 : 1;
+		unsigned short b14 : 1;
+		unsigned short b15 : 1;
+		unsigned short b16 : 1;
+		unsigned short b17 : 1;
+		unsigned short b18 : 1;
+		unsigned short b19 : 1;
+		unsigned short b20 : 1;
+		unsigned short b21 : 1;
+		unsigned short b22 : 1;
+		unsigned short b23 : 1;
+		unsigned short b24 : 1;
+		unsigned short b25 : 1;
+		unsigned short b26 : 1;
+		unsigned short b27 : 1;
+		unsigned short b28 : 1;
+		unsigned short b29 : 1;
+		unsigned short b30 : 1;
+		unsigned short b31 : 1;
+		unsigned short b32 : 1;
+	} asBits;
+    unsigned int asUInt;
+};
+
 #endif /* BITWISE_H_ */
--- src/randoms.cu
+++ src/randoms.cu
@@ -13,10 +13,21 @@ __global__ void initialize_food(unsigned int* theRandoms, float* target, float r
 __global__ void initialize_agentbits(unsigned int* theRandoms, int* target)
 {
 	int iAgentID = threadIdx.x + blockIdx.x*blockDim.x;
+	BitUnpacker buTemp;
 	AgentBitWise abwTemp;
-	abwTemp.asInt = theRandoms[iAgentID]-INT_MIN;
+	buTemp.asUInt = theRandoms[iAgentID];
 	
-	//TODO: fix unpacking of bits
+	abwTemp.asBits.isFemale = buTemp.asBits.b1;
+	abwTemp.asBits.vision = buTemp.asBits.b2+2*buTemp.asBits.b3;
+	abwTemp.asBits.metSugar = buTemp.asBits.b4+2*buTemp.asBits.b5;
+	abwTemp.asBits.metSpice = buTemp.asBits.b6+2*buTemp.asBits.b7;
+	abwTemp.asBits.startFertilityAge = buTemp.asBits.b8+2*buTemp.asBits.b9;
+	abwTemp.asBits.endFertilityAge = buTemp.asBits.b10+2*buTemp.asBits.b11+4*buTemp.asBits.b12+8*buTemp.asBits.b13;
+	abwTemp.asBits.deathAge = buTemp.asBits.b14+2*buTemp.asBits.b15+4*buTemp.asBits.b16+8*buTemp.asBits.b17+16*buTemp.asBits.b18;
+	abwTemp.asBits.pad = 0;
+	abwTemp.asBits.isLocked = 0;
+	float fTemp = buTemp.asBits.b8+2*buTemp.asBits.b9+4*buTemp.asBits.b10+8*buTemp.asBits.b11+16*buTemp.asBits.b12+32*buTemp.asBits.b13+64*buTemp.asBits.b4;
+	abwTemp.asBits.age = fTemp*100.0f/127.0f;
 	
 	target[iAgentID] = abwTemp.asInt;
 }
@@ -32,13 +43,20 @@ __global__ void fill_positions(unsigned int* theRandoms, short* psaX, short* psa
 __global__ void initialize_gridbits(unsigned int* theRandoms, int* target)
 {
 	int iAgentID = threadIdx.x + blockIdx.x*blockDim.x;
+	BitUnpacker buTemp;
+	buTemp.asUInt = theRandoms[iAgentID];
+
 	// pack bit fields separately
 	GridBitWise gbwBits;
-	gbwBits.asInt = theRandoms[iAgentID]-INT_MIN;
-	//TODO: fix unpacking of bits
-
 	gbwBits.asBits.isLocked = 0;
 	gbwBits.asBits.occupancy = 0;
+	float fTemp = buTemp.asBits.b1+2*buTemp.asBits.b2+4*buTemp.asBits.b3+8*buTemp.asBits.b4;
+	gbwBits.asBits.sugar = fTemp*10.0f/15.0f;
+	gbwBits.asBits.maxSugar = gbwBits.asBits.sugar;
+	fTemp = buTemp.asBits.b5+2*buTemp.asBits.b6+4*buTemp.asBits.b7+8*buTemp.asBits.b8;
+	gbwBits.asBits.spice = fTemp*10.0f/15.0f;
+	gbwBits.asBits.maxSpice = gbwBits.asBits.spice;
+	gbwBits.asBits.pad = 0;
 
 	target[iAgentID] = gbwBits.asInt;
 }
